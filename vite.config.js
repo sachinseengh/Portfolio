58dff87ee0a1f9f8bb9c498b29f3800d6dcf8cc8
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2025-01-29 17:54:35 +0545

    config modified

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",  // Make sure this matches your repo name!
+  base: "/",  // Make sure this matches your repo name!
 });
