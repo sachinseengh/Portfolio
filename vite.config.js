00b76dc949558dd3250094ce4d719af7bd7c9b35
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2025-01-29 19:35:14 +0545

    config added

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// Environment-based base path
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   base: "/",  // Make sure this matches your repo name!
->>>>>>> 58dff87ee0a1f9f8bb9c498b29f3800d6dcf8cc8
+
 });
