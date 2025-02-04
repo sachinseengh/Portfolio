b41655d6afa5cbc142fccb2d0f333ec8585e5f92
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2025-02-04 22:03:20 +0545

    underdevelopment notice added

--- src/components/UnderDevelopment.jsx
+++ src/components/UnderDevelopment.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UnderDevelopment = () => {
   return (
     <div className='text-center mt-20'>
-        <span className='text-center text-2xl  text-orange-500'>Site is under Development</span>
+        <span className='text-center text-2xl  text-orange-500'>Site is Currently under Development</span>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // Environment-based base path
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? './' : '/portfolio',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/portfolio',
 });
